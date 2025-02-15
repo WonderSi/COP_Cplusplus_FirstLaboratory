00311b0ae753ac37a65bde646b65f4cf7401187a
WonderSi <kirillkozlov00@gmail.com>
2025-02-15 13:06:05 +0700

    create interface Matrix and adding small fields in Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -6,3 +6,18 @@
 #define COP_CPLUSPLUS_FIRSTLABORATORY_MATRIX_H
 
 #endif //COP_CPLUSPLUS_FIRSTLABORATORY_MATRIX_H
+
+#pragma once
+
+#include <iostream>
+
+class Matrix {
+private :
+    int rows_, cols_;
+    double **matrix;
+
+public:
+    Matrix();
+    Matrix(int rows, int cols);
+    ~Matrix();
+};
