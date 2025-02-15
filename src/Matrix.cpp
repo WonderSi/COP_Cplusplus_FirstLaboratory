d2e05680687ffc4658152a8712b48434498bd3be
WonderSi <kirillkozlov00@gmail.com>
2025-02-16 02:07:02 +0700

    create operation EqMatrix, SumMatrix and create operator indexation

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -4,6 +4,7 @@
 
 #include "../include/Matrix.h"
 
+// Memory
 void Matrix::memoryAllocation() {
     matrix_ = new double *[rows_];
     for (int i = 0;i < rows_; i++) {
@@ -19,7 +20,7 @@ void Matrix::memoryDeallocation() {
 }
 
 
-
+// Constructor | Destructor
 Matrix::Matrix() {
     rows_ = 0;
     cols_ = 0;
@@ -36,8 +37,44 @@ Matrix::~Matrix() {
     memoryDeallocation();
 }
 
+// Operation
+bool Matrix::EqMatrix(const IMatrix& other) {
+    if (rows_ != other.getRows() || cols_ != other.getCols()) {
+        return false;
+    }
+    for (int i = 0; i < rows_; i++) {
+        for (int j = 0; j < cols_; j++) {
+            if (matrix_[i][j] != other(i,j)) {
+                return false;
+            }
+        }
+    }
+}
+
+void Matrix::SumMatrix(const IMatrix& other) {
+    if (rows_ != other.getRows() || cols_ != other.getCols()) {
+        throw std::invalid_argument("Different dimensions of the matrices");
+    }
+    for (int i = 0; i < rows_; i++) {
+        for (int j = 0; j < cols_; j++) {
+            matrix_[i][j] += other(i,j);
+        }
+    }
+}
+
+// Operator
 double& Matrix::operator()(int row, int col) {
-    if ()
+    if (row < 0 || row >= rows_ || col < 0 || col >= cols_) {
+        throw std::out_of_range("out_of_range");
+    }
+    return matrix_[row][col];
+}
+
+const double& Matrix::operator()(int row, int col) const {
+    if (row < 0 || row >= rows_ || col < 0 || col >= cols_) {
+        throw std::out_of_range("out_of_range");
+    }
+    return matrix_[row][col];
 }
 
 
